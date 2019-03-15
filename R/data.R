396c50c7e609bd8dc7222bd29ad955b01617a568
Yufree <yufree@live.cn>
2019-03-15 00:29:46 -0400

    add data document

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Demo mzrt list object
+#' @format mzrt list object
+#' @usage data(mzrt)
+"mzrt"
+
+#' Demo ipo parameters object
+#' @format ipo parameters
+#' @usage data(para)
+"para"
+
+#' Demo xcmseic object
+#' @format xcmseic object
+#' @usage data(srmeic)
+"srmeic"
+
+#' Demo xcmsset object
+#' @format xcmsset object
+#' @usage data(srmxset)
+"srmxset"
